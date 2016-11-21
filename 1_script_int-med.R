89947b8e732a29864ce9d6c34edc59244d862379
Brooke Charbonneau <charbonb@uoguelph.ca>
2016-11-20 20:13:06 -0500

    sun commit

--- 1_script_int-med.R
+++ 1_script_int-med.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(apaTables)
+library(MBESS)
 
 #load data
 my_data <- read_csv("lectureData6060.csv")
@@ -7,8 +8,16 @@ glimpse(my_data)
 
 analytic.data <- my_data
 
+apa.cor.table(analytic.data, table.number = 1)
+
 
 ##Do anxiety and preparation interact to predict exam scores?
+
+#ONLY keep columsn we need
+analytic.data <- my_data %>% select(Exam,Anxiety,Preparation)
+#keep complete cases only, list-wise deletion of cases
+analytic.data <- na.omit(analytic.data)
+
 
 #Center variables
 analytic.data <- analytic.data %>% mutate(x.centered=as.numeric(scale(Anxiety, center=T, scale=F)))
@@ -20,14 +29,22 @@ interaction.regression <- lm(Exam ~ x.centered + z.centered + I(x.centered*z.cen
                              data=analytic.data, na.action=na.exclude)
 #na.action=na.exclude = if any row has a missing value, drop the row (can't make the interaction product)
 
+#I get the values for the text this way
+summary(interaction.regression)
+
 #regression table
-apa.reg.table(interaction.regression)
-#or can use block approach
+apa.reg.table(interaction.regression, table.number = 2)
+#although sr2 is significant, its CI overlaps with 0 - inconsistent messaging = small effect
+#explore further
+
+
+#OR can use block approach
 block1 <- lm(Exam ~ x.centered + z.centered,data=analytic.data, na.action=na.exclude)
 block2 <- lm(Exam ~ x.centered + z.centered + I(x.centered*z.centered), data=analytic.data, na.action=na.exclude)
 apa.reg.table(block1, block2)
 #then look at delta R2 - will give same value as reg table approach
-#although R2 is significant, its CI overlaps with 0 - inconsistent messaging = small effect
+#although delta R2 is significant, its CI overlaps with 0 - inconsistent messaging = small effect
+#explore further
 
 
 ##make the graph - getting the lines on the surface (+1 SD)
@@ -38,13 +55,16 @@ analytic.data <- analytic.data %>% mutate(z.centered.at.plus.1SD = z.centered - 
 #This may seem counter intuitive, but we lower the scores to increase the zero point to +1
 
 #get formula
-simple.slope.plus.1SD <- lm(Exam ~ x.centered + z.centered.at.plus.1SD + I(x.centered*z.centered.at.plus.1SD),
+simple.slope.plus.1SD <- lm(Exam ~ x.centered + z.centered.at.plus.1SD 
+                            + I(x.centered*z.centered.at.plus.1SD),
                             data=analytic.data, na.action=na.exclude) 
+#values for text this way
 summary(simple.slope.plus.1SD)
 
 #make regression table
 apa.reg.table(simple.slope.plus.1SD)
 #drop last 2 lines - look only at x.centered and intercept lines
+
 
 
 ##make the graph - getting the lines on the surface (-1 SD)
@@ -54,17 +74,94 @@ analytic.data <- analytic.data %>% mutate(z.centered.at.minus.1SD = z.centered +
 #This may seem counter intuitive, but we increase the scores to decrease the zero point to -1
 
 #get formula
-simple.slope.minus.1SD <- lm(Exam ~ x.centered + z.centered.at.minus.1SD + I(x.centered*z.centered.at.minus.1SD),
+simple.slope.minus.1SD <- lm(Exam ~ x.centered + z.centered.at.minus.1SD  
+                            + I(x.centered*z.centered.at.minus.1SD),
                             data=analytic.data, na.action=na.exclude) 
-summary(simple.slope.minus.1SD)
 
 #make regression table
 apa.reg.table(simple.slope.minus.1SD)
 #drop last 2 lines - look only at x.centered and intercept lines
 
 
-##make the graph - plot 
-#continue from packages Brooke
+#values for text this way
+summary(simple.slope.minus.1SD)
+
+
+
+##  Figure 1: 3D Plot
+
+# See: summary(interaction.regression) for numbers to input here
+library(MBESS)
+sd.x <- sd(analytic.data$x.centered, na.rm=TRUE)
+intr.plot(b.0=55.5794,b.x=-2.1503,b.z=4.5648,b.xz=0.9077,
+          x.min=-2*sd.x,x.max=2*sd.x,z.min=-2*sd.z,z.max=2*sd.z,
+          xlab="Anxiety Centered",zlab="Preparation Centered",ylab="Exam Score",
+          expand=1,hor.angle=60,gray.scale=TRUE, line.wd=4,zlim=c(0,100)) 
+
+#save using menu's in RStudio. Manually include in MSWord Document
+
+
+
+##  Figure 2: 2D Plot
+
+# 2D Graph Range on X-Axis
+
+sd.x <- sd(analytic.data$x.centered, na.rm=TRUE)
+
+#graph from -1SD on Anxiety to +1SD Anxiety for a given level of Prepartion (+/-2 SD)
+x.axis.range <-seq(-2*sd.x,2*sd.x,by=.25*sd.x) #this indicates range on the x-axis -2SD to +2SD Anxiety
+
+
+# 2D Graph Lines for +1 SD on Prep and -1SD on Prep
+
+#level of z (+/- 1SD on Preparation)
+sd.z<-sd(analytic.data$z.centered, na.rm=TRUE)
+z.line.hi=  1*sd.z
+z.line.lo= -1*sd.z
+
+
+# 2D Graph: Create Data for Drawing Lines
+#+1SD Line
+predictor.x.range.line.hi <- expand.grid(x.centered=x.axis.range, z.centered=z.line.hi)
+y.values.at.plus.1SD.z <- predict(interaction.regression,newdata=predictor.x.range.line.hi)
+
+#-1SD Line
+predictor.x.range.line.lo <- expand.grid(x.centered=x.axis.range, z.centered=z.line.lo) 
+y.values.at.minus.1SD.z <- predict(interaction.regression,newdata=predictor.x.range.line.lo)
+
+#Put the information describing the lines into a data frame
+line.data <- data.frame(x.axis.range, y.values.at.plus.1SD.z, y.values.at.minus.1SD.z)
+
+
+
+
+# 2D Graph: Make the graph using above data 
+
+my.plot <- ggplot(line.data, aes(x=x.axis.range, y=y.values.at.plus.1SD.z)) 
+
+#make +1 SD Z line (because it is the one in the aes statement above)
+my.plot <- my.plot + geom_line(color="black",linetype="dotted",size=1.5) 
+
+#make the -1 SD Z line
+my.plot <- my.plot + geom_line(aes(x=x.axis.range,y=y.values.at.minus.1SD.z), 
+                               color="black",linetype="solid",size=1.5) 
+
+#set APA part of graph below
+my.plot <- my.plot + theme_classic(18)
+
+#labels
+my.plot <- my.plot + labs(x="Anxiety (mean centered)", y="Exam Grade")
+my.plot <- my.plot+annotate("text", x = -1, y = 68.5, label = "+1 SD Preparation")
+my.plot <- my.plot+annotate("text", x = -1, y =43.5, label = "-1 SD Preparation")
+
+#the SD of Anxiety (see Table 1 is 2.00 so -1SD to +1SD is -2 to 2)
+my.plot <- my.plot + coord_cartesian(xlim=c(-2,2),ylim=c(0,100))
+
+print(my.plot)
+#save using menu's in RStudio. Manually include in MSWord Document
+
+
+
 
 
 
